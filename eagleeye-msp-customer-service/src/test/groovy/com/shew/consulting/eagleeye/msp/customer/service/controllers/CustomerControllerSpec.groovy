2036c099f29d5d58b2972be7c6358e7807e8d9bb
Andrew Norman <Andrew.C.Norman@HealthPartners.com>
2020-07-14 18:22:06 -0500

    Change to dirty context before each test method.

--- eagleeye-msp-customer-service/src/test/groovy/com/shew/consulting/eagleeye/msp/customer/service/controllers/CustomerControllerSpec.groovy
+++ eagleeye-msp-customer-service/src/test/groovy/com/shew/consulting/eagleeye/msp/customer/service/controllers/CustomerControllerSpec.groovy
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class CustomerControllerSpec extends Specification {
 
     @Autowired
