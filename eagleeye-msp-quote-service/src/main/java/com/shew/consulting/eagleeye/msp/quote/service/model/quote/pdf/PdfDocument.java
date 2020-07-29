157c47f4ea0b00a37b3ab38387f3ca3c57d9a232
david <davidkosal01@gmail.com>
2020-07-28 20:38:16 -0500

    Added server device and patch management

--- eagleeye-msp-quote-service/src/main/java/com/shew/consulting/eagleeye/msp/quote/service/model/quote/pdf/PdfDocument.java
+++ eagleeye-msp-quote-service/src/main/java/com/shew/consulting/eagleeye/msp/quote/service/model/quote/pdf/PdfDocument.java
@@ -7,6 +7,8 @@ import com.shew.consulting.eagleeye.msp.quote.service.model.quote.Quote;
 import com.shew.consulting.eagleeye.msp.quote.service.model.quote.pdf.management.PdfCustomer;
 import com.shew.consulting.eagleeye.msp.quote.service.model.quote.pdf.management.PdfLogo;
 import com.shew.consulting.eagleeye.msp.quote.service.model.quote.pdf.management.pc.*;
+import com.shew.consulting.eagleeye.msp.quote.service.model.quote.pdf.management.server.PdfServerDeviceMonitoring;
+import com.shew.consulting.eagleeye.msp.quote.service.model.quote.pdf.management.server.PdfServerPatchManagement;
 import com.shew.consulting.eagleeye.msp.quote.service.model.services.Service;
 import lombok.Data;
 
@@ -65,6 +67,12 @@ public class PdfDocument {
         // Server management
         document.add(new Paragraph("Server Management", getFont14Bold()));
         newLine(document, 1);
+
+        new PdfServerDeviceMonitoring(quote, document, services);
+        newLine(document, 2);
+
+        new PdfServerPatchManagement(quote, document, services);
+        newLine(document, 2);
 
         document.close();
     }
---
+++ eagleeye-msp-quote-service/src/main/java/com/shew/consulting/eagleeye/msp/quote/service/model/quote/pdf/management/server/PdfServerPatchManagement.java
@@ -0,0 +1,37 @@
+package com.shew.consulting.eagleeye.msp.quote.service.model.quote.pdf.management.server;
+
+import com.itextpdf.text.Document;
+import com.shew.consulting.eagleeye.msp.quote.service.model.quote.Quote;
+import com.shew.consulting.eagleeye.msp.quote.service.model.quote.pdf.utils.PdfTableBuilder;
+import com.shew.consulting.eagleeye.msp.quote.service.model.services.Service;
+
+import java.util.Map;
+
+public class PdfServerPatchManagement {
+
+    private final Quote quote;
+    private final Document document;
+    private final Map<String, Service> services;
+    private final PdfTableBuilder builder;
+
+    public PdfServerPatchManagement(Quote quote, Document document, Map<String, Service> services) throws Exception {
+        this.quote = quote;
+        this.document = document;
+        this.services = services;
+        this.builder = new PdfTableBuilder(5);
+        init();
+    }
+
+    private void init() throws Exception {
+        builder.withQuote(quote)
+               .withServices(services)
+               .withDocument(document)
+               .withHeaders("Workstation")
+               .withRow("PatchManagementServer")
+               .withQuantitySubRow("PatchManagementWindowsUpdatesServer")
+               .withQuantitySubRow("PatchManagementOfficeUpdatesServer")
+               .withQuantitySubRow("PatchManagementOtherUpdatesServer")
+               .build();
+    }
+
+}
